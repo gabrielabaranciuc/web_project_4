1cd906e0769b934e3fdc8f1f448e9a0e08e70f39
Gabriela Baranciuc <gabrielabaranciuc1@gmail.com>
2021-01-04 08:34:33 -0500

    feat: add scripts modal section

---
+++ scripts/scripts.js
@@ -0,0 +1,28 @@
+
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.modal__reset-button');
+let form = document.querySelector('.form');
+let modal = document.querySelector('.modal');
+
+let nameInput = document.querySelector('.form__input_name');
+let titleInput = document.querySelector('.form__input_title');
+let profileName = document.querySelector('.profile__name');
+let profileTitle = document.querySelector('.profile__title');
+
+form.addEventListener('submit', function(Event) {
+    Event.preventDefault();
+
+    profileName.textContent  = nameInput.value;
+    profileTitle.textContent  = titleInput.value;
+
+    toggleModal()
+})
+
+function toggleModal() {
+    
+    modal.classList.toggle('modal_opened');
+}
+
+editButton.addEventListener('click', toggleModal)
+
+closeButton.addEventListener('click', toggleModal)
