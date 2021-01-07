6ea5af1cd543a7926e771c60c5524d019024dcf1
Gabriela Baranciuc <gabrielabaranciuc1@gmail.com>
2021-01-07 12:03:17 -0500

    hotfix/scripts-changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,28 +1,32 @@
 
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.modal__reset-button');
-let form = document.querySelector('.form');
 let modal = document.querySelector('.modal');
+let form = document.querySelector('.form');
+let saveButton = editButton.querySelector('.form__submit-button');
 
 let nameInput = document.querySelector('.form__input_type_name');
 let titleInput = document.querySelector('.form__input_type_title');
 let profileName = document.querySelector('.profile__name');
 let profileTitle = document.querySelector('.profile__title');
 
-form.addEventListener('submit', function(Event) {
-    Event.preventDefault();
+function toggleModal () {
+    nameInput.value = profileName.textContent;
+    titleInput.value = profileTitle.textContent;
+    modal.classList.toggle('modal_opened');
+}
+
+function formSubmitHandler(e) {
+    e.preventDefault();
 
     profileName.textContent  = nameInput.value;
     profileTitle.textContent  = titleInput.value;
-
-    toggleModal();
-})
-
-function toggleModal() {
     
-    modal.classList.toggle('modal_opened');
+    toggleModal();
 }
 
 editButton.addEventListener('click', toggleModal);
 
 closeButton.addEventListener('click', toggleModal);
+
+form.addEventListener('submit', formSubmitHandler);
